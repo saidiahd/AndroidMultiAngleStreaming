8e0c02ac494f038a02ec88571d2645230cbce764
Farouk <farouk.bengarssallah@gmail.com>
2014-08-10 20:33:01 +0100

    Second Version
    Create the VideoMultiView classe, the setVideo Uris
    The POM dependency for the android 4.1.4 has been removed, tis was
    replaced by the android 4.4.4 JAR. It can be downloaede by this link
    
    http://repository.grepcode.com/java/ext/com/google/android/android/4.4.4_r1/android-4.4.4_r1.jar

--- Android/src/com/google/android/farouk/video/VideoMultiView.java
+++ Android/src/com/google/android/farouk/video/VideoMultiView.java
@@ -1,7 +1,33 @@
 package com.google.android.farouk.video;
 
-import com.
+import java.util.Map;
 
-public class VideoMultiView {
+import android.content.Context;
+import android.net.Uri;
+import android.widget.VideoView;
 
+
+     /** <p>Displays a video file. The {@link VideoMultiView} class can load video content  
+      * from various sources (such as resources or content providers), 
+      * takes care of computing its measurement from the video so that it can be
+      *  used in any layout manager, and provides various display options such as
+      *   scaling and tinting.<p>
+     **/
+
+public class VideoMultiView extends VideoView {
+	
+	  // the video URIs list, which the user which can switch between 
+	  // them
+	private Uri[] mURIs;
+
+	public VideoMultiView(Context context) {
+		super(context);
+	}
+	
+	public void setVideoURIs(Uri[]uri, Map<String, String>headers){
+		  mURIs = uri;
+		  
+	}
+
+	
 }
